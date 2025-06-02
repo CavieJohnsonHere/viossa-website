67784891e027587d47dcc47e312a144fac5e9b90
Mohammad Dargahi <caviejohnsonhere@gmail.com>
2025-06-02 11:30:26 +0330

    fixed infoboxes not filling up the whole width

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -6,16 +6,16 @@ export default function Comumnity() {
   return (
     <div className="p-5 text-white">
       <Slideshow />
-      <div className="my-5 mx-auto w-fit flex gap-5">
-        <Infobox title="What are these?">
+      <div className="my-5 w-full flex gap-5">
+        <Infobox title="What are these?" className="max-w-none">
           Community events are opportunities to hang out with fellow
           Viossa-speaking friends.
         </Infobox>
-        <Infobox title="What are these?">
+        <Infobox title="What are these?" className="max-w-none">
           Community events are opportunities to hang out with fellow
           Viossa-speaking friends.
         </Infobox>
-        <Infobox title="What are these?">
+        <Infobox title="What are these?" className="max-w-none">
           Community events are opportunities to hang out with fellow
           Viossa-speaking friends.
         </Infobox>
--- src/app/components/infobox.tsx
+++ src/app/components/infobox.tsx
@@ -2,13 +2,15 @@ export default function Infobox({
   children,
   title,
   image,
+  className,
 }: {
   children: string;
   title: string;
   image?: { href: string; alt: string; right?: boolean };
+  className?: string;
 }) {
   return (
-    <div className="w-full max-w-125 bg-viossa-800 border border-viossa-700 rounded-2xl p-5 px-6 text-viossa-50">
+    <div className={`w-full max-w-125 bg-viossa-800 border border-viossa-700 rounded-2xl p-5 px-6 text-viossa-50 ${className}`}>
       {image ? (
         <div className="grid grid-cols-4 gap-5">
           {image.right ? (
